1ff714e31532047fb910cfc0e845e5ec3225da6a
Irving Gomar <icgomar@gmail.com>
2024-01-19 19:15:59 -0600

    agregamos role al formulario de registro

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+enum role: {
+  0 => "ONG",
+  1 => "Voluntario",
+  2 => "Migrante"
+}
 end
