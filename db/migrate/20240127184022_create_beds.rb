119d940b26108773e7841fd7c3ed74ffab4d55d1
Barbara Ruiz <barbararuiz456@outlook.com>
2024-02-03 16:35:41 -0600

    errores de migraciones

--- db/migrate/20240127184022_create_beds.rb
+++
@@ -1,8 +0,0 @@
-class CreateBeds < ActiveRecord::Migration[7.0]
-  def change
-    create_table :beds do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-User.create(email: "contacto@infoappmex.org", password: 123456, role: "ONG")
+require "open-uri"
+
+contacto = User.create!(email: "contacto@infoappmex.org", password: 123456, role: "ONG")
+
+ong1_user = User.create!(email: "ong1@infoappmex.org", password: 123456, role: "ONG")
+
+
+file = URI.open("https://pbs.twimg.com/profile_images/984958576614694913/LRu6zJV8_400x400.jpg")
+ong_acnur = Ong.create!(name: "ACNUR", description: "Nuestro objetivo principal en ACNUR es salvaguardar los derechos y el bienestar de las personas que se han visto obligadas a huir.
+  Junto con socios y comunidades, trabajamos para garantizar que todas las personas tengan derecho a solicitar asilo y encontrar un refugio seguro en otro país. También nos esforzamos por asegurar soluciones duraderas.", address: "Calz. General Mariano Escobedo 526
+  Chapultepec Morales, Anzures
+  11590 Miguel Hidalgo, Ciudad de México, CDMX", user: ong1_user)
+ong_acnur.photo.attach(io: file, filename: "acnur.png", content_type: "image/png")
+ong_acnur.save
