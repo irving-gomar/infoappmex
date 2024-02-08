b0d1855f4b8b5e7c0b98645cf5a5dfbb9ac7e619
Irving Gomar <icgomar@gmail.com>
2024-02-07 22:06:32 -0600

    service controller y seeds

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,8 @@ module Infoappmex
       generate.test_framework :test_unit, fixture: false
     end
     # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.default_locale = :es
     config.load_defaults 7.0
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
