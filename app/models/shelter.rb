545bd4ca92229d53990bcb1f3afff7ddbf344b6c
Barbara Ruiz <barbararuiz456@outlook.com>
2024-02-10 17:53:02 -0600

    vistas listas, ong done :D

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,6 +1,7 @@
 class Shelter < ApplicationRecord
   belongs_to :ong
-  has_many :beds
+  has_many :beds, dependent: :destroy
+
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
