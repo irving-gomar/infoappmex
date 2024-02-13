646bd30b34f9353ff106366faed2afa8f0160c03
Barbara Ruiz <barbararuiz456@outlook.com>
2024-02-12 20:14:11 -0600

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   validates :role, presence: true, inclusion: { in: %w[ONG Voluntario Migrante] }
 
+  has_one_attached :photo
   has_many :messages
   has_many :volunteerings
   has_many :bookings
